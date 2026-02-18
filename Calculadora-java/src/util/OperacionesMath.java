4ca7662a3d73e2e823f1c15f1f444ccaf34a75c6
unknown <danielfepilelg2006@gmail.com>
2026-02-18 18:55:24 -0500

    diagrama de clases aumentado

--- Calculadora-java/src/util/OperacionesMath.java
+++ Calculadora-java/src/util/OperacionesMath.java
@@ -26,6 +26,18 @@ public class OperacionesMath {
     }
 
     public double division(double... numeros) {
+        if(numeros.length == 2){
+            if (numeros[1] == 0) {
+                System.out.println("No se puede dividir por cero");
+                return 0;
+            }else{
+                double division = numeros[0];
+                for(int i = 1; i<numeros.length; i++){
+                    division /= numeros[i];
+                }
+                return division;
+            }
+        }
         if (numeros[1] == 0 || numeros[2] == 0) {
             System.out.println("No se puede dividir por cero");
             return 0;
